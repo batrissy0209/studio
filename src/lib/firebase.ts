674c0613f98c7b48e33ad73b47edb106a9a2f8ba
Batrisyia Izzati <batrissy0209@gmail.com>
2025-09-21 12:37:16 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA61Sr0jzDrGbl6BGn89RmWVmbFd9csPB8",
+  authDomain: "studio-717135246-44200.firebaseapp.com",
+  projectId: "studio-717135246-44200",
+  storageBucket: "studio-717135246-44200.appspot.com",
+  messagingSenderId: "450512461738",
+  appId: "1:450512461738:web:55b637280a4671118a15d3",
 };
 
 // Initialize Firebase
